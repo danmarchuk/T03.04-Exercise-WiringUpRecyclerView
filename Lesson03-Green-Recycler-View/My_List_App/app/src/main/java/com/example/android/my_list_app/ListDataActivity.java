989bc86ad76b260a3d4fb9673fa3cc113aa689f1
Danyil Marchuk <zemero58@gmail.com>
2020-12-30 00:50:26 +0100

    initial commit

---
+++ Lesson03-Green-Recycler-View/My_List_App/app/src/main/java/com/example/android/my_list_app/ListDataActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.my_list_app;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+class ListDataActivity extends AppCompatActivity {
+
+    private static final String TAG = "ListDataActivity";
+    DatabaseHelper mDatabaseHelper;
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_layout);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+
+    }
+    private void populateListView() {
+        Cursor data = mDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+        while (data.moveToNext()){
+            listData.add(data.getString(1)); }
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listData);
+        mListView.setAdapter(adapter);
+    }
+}
